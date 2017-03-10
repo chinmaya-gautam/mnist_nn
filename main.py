412d6c0fdc54eb2a6de7239705bdf1b0c546b1be
Chinmaya Gautam <chinmaya.gautam@gmail.com>
2017-03-10 00:25:00 -0800

    added classification model

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ np.set_printoptions(threshold='nan')
 
 class data:
     train_df = pd.read_csv('train.csv', sep=',', header=0)
-
+    
     #normalize data and store in numpy array
     train_inputs = np.array(train_df.ix[:,1:].apply(lambda x:x/max(x), axis=1))
     #train_inputs = np.array(train_df.ix[:,1:])
-    train_labels = np.array(train_df['label'])
-    
+    temp = np.array(train_df['label'])
+    classifier_labels = np.zeros([np.size(temp), 10])
+    for i in range(np.size(temp)):
+        classifier_labels[i][temp[i]] = 1
+    del temp
+
 class visualize:
     def __init__(self):
         pass
@@ -66,7 +70,11 @@ class nn:
         # initialize a random model
         # model = weight matrix or dimensions number of visible layers by 
         # number of hidden layers
+        # x2 -1 so that it has 0 mean, 0.1 multiplier to make it small
         self.model = (np.random.random([self.num_vis, self.num_hid]) *2.0 -1.0) *0.1
+
+        #classifier model has inputs as hiddern representation learned by rbm and output is 10 way softmax
+        self.classifier_model = (np.random.random([self.num_hid, 10]) *2.0 -1.0) *0.1
 
     def sigmoid(self, z):
         def _sigmoid(a):
@@ -137,6 +145,55 @@ class nn:
             #print self.model
             self.model = self.model + self.lr_rbm * momentum_speed
 
+
+    def softmax(self, data):
+        #shape of data: <#training_cases> x <#classes>
+        denominator = np.log(np.sum(np.exp(data),1))
+        temp = data - np.reshape(denominator, [np.size(denominator), 1])
+        ans = np.exp(temp)
+        #print "shape of data:", np.shape(data)
+        #print "shape of denominator:", np.shape(denominator)
+        #print "shape of ans:", np.shape(ans)
+        return ans
+
+    def classifier_gradient(self, inputs, labels):
+        #shape of inputs: <#training_cases> x <#inputs> 
+        #                (inputs for classification model corresponds to hidden layer of rbm)
+        #shape of labels: <#training_cases> x <#classes>
+        #shape of model: <#inputs> x <#classes>
+        #shape of gradient: <#inputs> x <#classes>
+
+        #forward pass
+        class_input = np.matmul(inputs, self.classifier_model)
+        class_prob = self.softmax(class_input)
+        
+        #backward pass
+        grad_at_class = -(labels - class_prob) / np.shape(inputs)[0]
+        grad_at_input = - np.matmul(inputs.T, grad_at_class)
+
+        return grad_at_input
+
+    def learn_classification_model(self):
+        start_of_next_mini_batch = 0
+        momentum_speed = np.zeros(np.shape(self.classifier_model))
+        #momentum_speed = np.random.random(np.shape(self.model))
+        print "learning rate(classification):", self.lr_classification
+        print "momentum:", self.momentum
+
+        for i in range(self.n_iters):
+            print "iteration num:", i
+            mini_batch_inputs = data.classifier_inputs[start_of_next_mini_batch:start_of_next_mini_batch+self.mini_batch_size, :]
+            mini_batch_labels = data.classifier_labels[start_of_next_mini_batch:start_of_next_mini_batch+self.mini_batch_size, :]
+            start_of_next_mini_batch = (start_of_next_mini_batch + self.mini_batch_size) % np.shape(data.train_inputs)[0]
+            gradient = self.classifier_gradient(mini_batch_inputs, mini_batch_labels)
+            momentum_speed = self.momentum * momentum_speed + gradient
+            self.classifier_model = self.classifier_model + self.lr_classification * momentum_speed
+        
+    def classify(self):
+        #learn the hidden layer representation
+        data.classifier_inputs = self.sigmoid(np.matmul(data.train_inputs, self.model))
+        self.learn_classification_model()
+
     def test_method(self):
         #test sampler
         #sample1 = self.sample(data.train_inputs[:self.mini_batch_size, :])
@@ -145,14 +202,18 @@ class nn:
         #return sample1
 
         #test cd1
-        new_model = self.cd1(self.model, data.train_inputs[:self.mini_batch_size, :])
-        print "shape of model:", np.shape(new_model)
-        return new_model
+        #new_model = self.cd1(self.model, data.train_inputs[:self.mini_batch_size, :])
+        #print "shape of model:", np.shape(new_model)
+        #return new_model
+
+        #test classify
+        self.classify()
 
 v = visualize()
 #v.show_img(data.train_inputs[1])
-s = nn(100,0.5,0.5,500,0.5,200)
+s = nn(100,0.5,0.5,10,0.5,200)
 s.learn_model()        
-v.show_learned_features(s.model)
+s.test_method()
+#v.show_learned_features(s.model)
 
 #print np.sum(s.test_method())
